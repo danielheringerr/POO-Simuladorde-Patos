b314d15a4abff2ec7f1632491714539c4d912b35
Mateus Botelho <116606952+botelllhx@users.noreply.github.com>
2025-05-09 14:27:28 -0300

    menu implementado no simulador de patos

--- Simulador de Patos/Program.cs
+++ Simulador de Patos/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Simulador_de_Patos
+using System;
+using System.Collections.Generic;
+
+namespace Simulador_de_Patos
 {
     internal class Program
     {
@@ -11,21 +14,57 @@
                 new RubberDuck()
             };
 
-            foreach (Duck pato in patos)
+            int indiceAtual = 0;
+            bool executando = true;
+
+            while (executando)
             {
-                pato.display();
-                pato.swim();
+                Console.Clear();
+                Console.WriteLine($"Pato {indiceAtual + 1} de {patos.Count}");
 
-                if (pato is IFlyable voador)
-                    voador.fly();
+                patos[indiceAtual].display();
 
-                if (pato is IQuackable quackable)
-                    quackable.quack();
+                Console.WriteLine("\n[ N ] Próximo  |  [ V ] Voltar  |  [ S ] Selecionar  |  [ F ] Finalizar");
+                Console.Write("Escolha uma opção: ");
+                string input = Console.ReadLine().ToUpper();
 
-                Console.WriteLine(); 
+                switch (input)
+                {
+                    case "N":
+                        indiceAtual = (indiceAtual + 1) % patos.Count;
+                        break;
+
+                    case "V":
+                        indiceAtual = (indiceAtual - 1 + patos.Count) % patos.Count;
+                        break;
+
+                    case "S":
+                        Console.Clear();
+                        Console.WriteLine("=== Informações do Pato Selecionado ===\n");
+                        patos[indiceAtual].display();
+                        patos[indiceAtual].swim();
+
+                        if (patos[indiceAtual] is IFlyable voador)
+                            voador.fly();
+
+                        if (patos[indiceAtual] is IQuackable quackable)
+                            quackable.quack();
+
+                        Console.WriteLine("\nPressione qualquer tecla para voltar ao menu...");
+                        Console.ReadKey();
+                        break;
+
+                    case "F":
+                        executando = false;
+                        Console.WriteLine("Programa finalizado.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida! Pressione qualquer tecla para tentar novamente.");
+                        Console.ReadKey();
+                        break;
+                }
             }
-
-            Console.ReadKey();
         }
     }
 }
